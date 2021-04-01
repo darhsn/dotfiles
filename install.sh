b9607cb097e6b19564ab49f76734f9332ec3bb22
Dario <76775977+darioisthebest@users.noreply.github.com>
2021-04-01 16:06:21 +0200

    Added installation script

--- install.sh
+++ install.sh
@@ -1,6 +1,21 @@
 echo "+== Dotfiles install script ==+"
 
-if [ "$(whoami)" != "root" ]; then
-    echo "ERR: Installation script requires root privileges"
-    exit
-fi
+echo "==> Installation scripts requires pacman and yay"
+
+cd
+rm -rf $HOME/dotfiles
+mkdir $HOME/dotfiles
+
+git clone http://github.com/darioisthebest/dotfiles $HOME/dotfiles
+
+# install dependencies
+${SUDO} pacman -S kitty polybar i3-gaps rofi nitrogen picom pfetch lf dunst doas base-devel
+
+# install aur helper (yay)
+git clone https://aur.archlinux.org/yay.git $HOME/yay
+${SUDO} chown -R  $(whoami):users $HOME/yay
+cd $HOME/yay
+makepkg -si
+
+# install aur packages
+yay -S neovim-git
