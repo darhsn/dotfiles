328a9fcc7892132e36d81550578ebde4d626b3d7
Dario <76775977+darioisthebest@users.noreply.github.com>
2021-04-02 11:00:45 +0200

    Added switch-theme script

--- switch-theme.sh
+++ switch-theme.sh
@@ -10,28 +10,28 @@ switch-theme() {
     elif [ "${1}" == "gruvbox" ]; then
         cd $DOTFILES_DIR
         cp polybar/gruvbox-config polybar/config
-        cp rofi/gruvbox-config.rasi rofi/config.rasi
+        cp rofi/gruvbox-config.rasi rofi/theme.rasi
         cp dunst/gruvbox-dunstrc dunst/dunstrc
         cd ..
         echo "switch-theme: changed theme to gruvbox, wallpaper and vim theme should be changed manually"
     elif [ "${1}" == "onedark" ]; then
         cd $DOTFILES_DIR
         cp polybar/onedark-config polybar/config
-        cp rofi/onedark-config.rasi rofi/config.rasi
+        cp rofi/onedark.rasi rofi/theme.rasi
         cp dunst/onedark-dunstrc dunst/dunstrc
         cd ..
         echo "switch-theme: changed theme to onedark, wallpaper and vim theme should be changed manually"
     elif [ "${1}" == "nord" ]; then
         cd $DOTFILES_DIR
         cp polybar/nord-config polybar/config
-        cp rofi/nord-config.rasi rofi/config.rasi
+        cp rofi/nord.rasi rofi/theme.rasi
         cp dunst/nord-dunstrc dunst/dunstrc
         cd ..
         echo "switch-theme: changed theme to nord, wallpaper and vim theme should be changed manually"
     elif [ "${1}" == "base16" ]; then
         cd $DOTFILES_DIR
         cp polybar/base16-config polybar/config
-        cp rofi/base16-config.rasi rofi/config.rasi
+        cp rofi/base16.rasi rofi/theme.rasi
         cp dunst/base16-dunstrc dunst/dunstrc
         cd ..
         echo "switch-theme: changed theme to base16, wallpaper and vim theme should be changed manually"
