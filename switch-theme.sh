dc595ec3989f3e268ab8b3a900993554be071c17
Dario <76775977+darioisthebest@users.noreply.github.com>
2021-04-27 15:57:11 +0200

    Themes

--- switch-theme.sh
+++ switch-theme.sh
@@ -8,33 +8,33 @@ switch-theme() {
         echo "switch-theme: usage: switch-theme THEME_NAME"
         echo "Themes: gruvbox, onedark, nord, base16"
     elif [ "${1}" == "gruvbox" ]; then
-        cd $DOTFILES_DIR
-        cp polybar/gruvbox-config polybar/config
-        cp rofi/gruvbox.rasi rofi/theme.rasi
-        cp dunst/gruvbox-dunstrc dunst/dunstrc
-        cp kitty/gruvbox-kitty.conf kitty/kitty.conf
+        cp $DOTFILES_DIR/polybar/gruvbox-config $DOTFILES_DIR/polybar/config
+        cp $DOTFILES_DIR/rofi/gruvbox.rasi $DOTFILES_DIR/rofi/theme.rasi
+        cp $DOTFILES_DIR/dunst/gruvbox-dunstrc $DOTFILES_DIR/dunst/dunstrc
+        cp $DOTFILES_DIR/kitty/gruvbox-kitty.conf $DOTFILES_DIR/kitty/kitty.conf
         echo "switch-theme: changed theme to gruvbox, wallpaper and vim theme should be changed manually"
     elif [ "${1}" == "onedark" ]; then
-        cd $DOTFILES_DIR
-        cp polybar/onedark-config polybar/config
-        cp rofi/onedark.rasi rofi/theme.rasi
-        cp dunst/onedark-dunstrc dunst/dunstrc
-        cp kitty/onedark-kitty.conf kitty/kitty.conf
+        cp $DOTFILES_DIR/polybar/onedark-config $DOTFILES_DIR/polybar/config
+        cp $DOTFILES_DIR/rofi/onedark.rasi $DOTFILES_DIR/rofi/theme.rasi
+        cp $DOTFILES_DIR/dunst/onedark-dunstrc $DOTFILES_DIR/dunst/dunstrc
+        cp $DOTFILES_DIR/kitty/onedark-kitty.conf $DOTFILES_DIR/kitty/kitty.conf
         echo "switch-theme: changed theme to onedark, wallpaper and vim theme should be changed manually"
     elif [ "${1}" == "nord" ]; then
-        cd $DOTFILES_DIR
-        cp polybar/nord-config polybar/config
-        cp rofi/nord.rasi rofi/theme.rasi
-        cp dunst/nord-dunstrc dunst/dunstrc
-        cp kitty/nord-kitty.conf kitty/kitty.conf
+        cp $DOTFILES_DIR/polybar/nord-config $DOTFILES_DIR/polybar/config
+        cp $DOTFILES_DIR/rofi/nord.rasi $DOTFILES_DIR/rofi/theme.rasi
+        cp $DOTFILES_DIR/dunst/nord-dunstrc $DOTFILES_DIR/dunst/dunstrc
+        cp $DOTFILES_DIR/kitty/nord-kitty.conf $DOTFILES_DIR/kitty/kitty.conf
         echo "switch-theme: changed theme to nord, wallpaper and vim theme should be changed manually"
     elif [ "${1}" == "base16" ]; then
-        cd $DOTFILES_DIR
-        cp polybar/base16-config polybar/config
-        cp rofi/base16.rasi rofi/theme.rasi
-        cp dunst/base16-dunstrc dunst/dunstrc
-        cp kitty/base16-kitty.conf kitty/kitty.conf
+        cp $DOTFILES_DIR/polybar/base16-config $DOTFILES_DIR/polybar/config
+        cp $DOTFILES_DIR/rofi/base16.rasi $DOTFILES_DIR/rofi/theme.rasi
+        cp $DOTFILES_DIR/dunst/base16-dunstrc $DOTFILES_DIR/dunst/dunstrc
+        cp $DOTFILES_DIR/kitty/base16-kitty.conf $DOTFILES_DIR/kitty/kitty.conf
         echo "switch-theme: changed theme to base16, wallpaper and vim theme should be changed manually"
+    else
+        echo "switch-theme: invalid theme!"
+        echo "switch-theme: usage: switch-theme THEME_NAME"
+        echo "Themes: gruvbox, onedark, nord, base16"
     fi
 
 }
